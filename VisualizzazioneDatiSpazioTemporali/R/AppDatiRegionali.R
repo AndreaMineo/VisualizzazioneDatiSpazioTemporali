be1e9d5deb25ef7f9f5feddb7fda5431bf512716
AndreaMineo <92448038+AndreaMineo@users.noreply.github.com>
2022-03-19 10:27:48 +0100

    Add files via upload

---
+++ VisualizzazioneDatiSpazioTemporali/R/AppDatiRegionali.R
@@ -0,0 +1,371 @@
+#' Applicazione per la visualizzazione di dati spazio temporali caso dati regionali
+#'
+#' @description Shiny app per la visualizzazione di dati spazio temporali.
+#' Per maggiori informazioni riferirsi a:
+#'
+#' https://github.com/AndreaMineo/VisualizzazioneDatiSpazioTemporali
+#' @export
+
+
+
+AppDatiRegionali <- function(){
+
+  ui <- shiny::fluidPage(
+
+    shiny::sidebarLayout(
+
+      shiny::sidebarPanel(
+
+
+
+        shiny::fileInput("dataFile", "Selecting data file",
+                         multiple = FALSE,
+                         accept = c("text/csv",
+                                    "text/comma-separated-values,text/plain",
+                                    ".csv","xlsx")),
+
+
+        shiny::fileInput(inputId = "filemap",
+                         label = "Upload map. Choose shapefile",
+                         multiple = TRUE,
+                         accept = c('.shp','.dbf','.shx','.prj',".RData")),
+
+
+
+
+        shiny::selectInput("colRegNameShapeFile",
+                           label= "Selecting the column containing regions' names in the shape file",
+                           choices=NULL
+        ),
+
+
+
+
+        shiny::selectInput("colRegNameDataFile",
+                           label= "Selecting the column containing regions' names in the data file",
+                           choices=NULL
+        ),
+
+
+
+        shiny::selectInput("variable",
+                           label= "Selecting the variable to plot",
+                           choices=NULL,
+                           selected = NULL
+        ),
+        shiny::selectInput("regToPlot",
+                           label= "Selecting the set of regions to plot for time series plot",
+                           choices=NULL,
+                           selected = NULL,
+                           multiple = TRUE
+        ),
+
+
+
+        shinyWidgets::sliderTextInput("ChoosedDate",
+                                      label="Selecting date for spatial plot",
+                                      choices=c("first_date","last_date"),
+                                      selected = NULL,
+                                      animate = TRUE
+        ),
+
+
+
+        shiny::textAreaInput(
+          inputId="ValuesForLegend",
+          label="Insert values to use in the legend of the spatial plot",
+          value = "",
+          placeholder = "value1,value2,...,valueN"
+        ),
+
+        shiny::selectInput("FormatDownloadSpatialPlot",label="Select format to use to download the Spatial Plot",choices = c("jpeg","pdf","html"),selected = "jpeg"),
+        shiny::downloadButton("DownloadSpatialPlot", label = "Download Spatial Plot"),
+
+
+        shiny::selectInput("FormatDownloadTimeSeriesPlot",label="Select format to use to download the Time Series Plot",choices = c("jpeg","pdf","html"),selected = "jpeg"),
+        shiny::downloadButton("DownloadTimeSeriesPlot", label = "Download Time Series Plot")
+
+
+
+      ),
+
+      shiny::mainPanel(
+
+        #### ERROR TEXT OUTPUT ####
+
+        shiny::textOutput("error"),
+
+        shiny::br(),shiny::br(),
+
+        #### TIME SERIES PLOT
+        dygraphs::dygraphOutput("TimeSeriesPlot"),
+
+        shiny::br(),shiny::br(),
+
+        ####SPATIAL PLOT
+        tmap::tmapOutput("SpatialPlot")
+      )
+    )
+  )
+
+
+
+  server <- function(input,output,session){
+
+    options(shiny.maxRequestSize=30*1024^2)
+
+
+    ### loading data file
+    dataFileName <- shiny::reactive({
+      shiny::req(input$dataFile)
+      input$dataFile
+    })
+
+
+    data <- shiny::reactive({
+      shiny::validate(
+        validate_dataFile(dataFileName()) %then%
+          validate_dataFormat(dataFileName())
+      )
+      loadDataFile(dataFileName())
+    })
+
+
+
+    ### loading shape file
+
+    shapeFileName <- shiny::reactive({
+
+      shiny::req(input$filemap)
+      input$filemap
+    })
+
+    map <- shiny::reactive({
+      shiny::validate(
+
+        validate_ShapeFile(shapeFileName())%then%
+          validate_mapFormat(shapeFileName())
+      )
+      loadShapeFile(shapeFileName())
+    })
+
+
+
+    DataColumns <- shiny::reactive({names(data())})
+
+    shiny::observeEvent(DataColumns(), {
+      shiny::updateSelectInput(inputId = "colRegNameDataFile", choices = DataColumns()[-1])
+    })
+
+    ShapeColumns <- shiny::reactive({names(map())})
+
+    shiny::observeEvent(ShapeColumns(), {
+      shiny::updateSelectInput(inputId = "colRegNameShapeFile", choices = ShapeColumns())
+    })
+
+
+
+    mapRegNameCol <- shiny::reactive({
+      shiny::req(input$colRegNameShapeFile)
+      input$colRegNameShapeFile
+    })
+
+
+    updatedMap <- shiny::reactive({
+      shiny::validate(
+        validate_mapRegNameCol(mapRegNameCol(),map())
+      )
+      renameColumn(map(),mapRegNameCol(),"region_name")
+    })
+
+
+
+
+    dataRegNameCol <- shiny::reactive({
+      shiny::req(input$colRegNameDataFile)
+      input$colRegNameDataFile
+
+    })
+
+    updatedData <- shiny::reactive({
+      shiny::validate(
+        validate_mapRegNameCol(mapRegNameCol(),map()) %then%
+          validate_dataRegNameCol(dataRegNameCol(),data(),updatedMap())
+      )
+      renameColumn(data(),dataRegNameCol(),"region_name")
+
+    })
+
+
+
+    shiny::observeEvent(updatedData(), {
+      values <- names(updatedData())[!names(updatedData()) %in% c("date","region_name")]
+      shiny::updateSelectInput(inputId = "variable", choices = values)
+      v <- c("all",unique(updatedData()$region_name))
+      shiny::updateSelectInput(inputId="regToPlot",choices=v,selected="all")
+    })
+
+
+    shiny::observeEvent(data(),{
+      s <- unique(data()[,1])
+      shinyWidgets::updateSliderTextInput(session=session,inputId = "ChoosedDate",choices = as.character(s))
+    })
+
+
+
+    date <- shiny::reactive({
+      shiny::req(input$ChoosedDate)
+      as.Date(input$ChoosedDate)
+
+    })
+
+    variable <- shiny::reactive({
+      shiny::req(input$variable)
+      shiny::validate(
+        validate_variable(input$variable,updatedData())
+      )
+      input$variable
+    })
+
+    setOfRegations <- shiny::reactive({
+
+      shiny::req(input$regToPlot)
+      input$regToPlot
+    })
+
+
+
+    shiny::observeEvent(variable(),{
+
+      values <- get_bins("",data(),variable())
+      shiny::updateTextAreaInput(inputId = "ValuesForLegend",value = values)
+    })
+
+    ValuesForLegend <- shiny::reactive({
+
+      shiny::req(input$ValuesForLegend)
+      shiny::validate(
+        validate_valuesForLegend(input$ValuesForLegend)
+      )
+      get_bins(input$ValuesForLegend,data(),variable())
+    })
+
+
+
+
+
+    dataForSpatialPlot <- shiny::reactive({
+      generateDataForSpatialPlotRegion(updatedData(),updatedMap(),variable(),date())
+    })
+
+    dataForTimeSeriesPlot <- shiny::reactive({
+      generateDataForTimeSeriesPlotRegion(updatedData(),variable(),setOfRegations())
+    })
+
+
+
+    TimeSeriesPlot <- shiny::reactive({
+
+      d <- dygraphs::dyRangeSelector(dygraphs::dygraph(dataForTimeSeriesPlot()))
+      dygraphs::dyLegend(d,show = 'onmouseover',width = 400)
+
+    })
+    output$TimeSeriesPlot <- dygraphs::renderDygraph({
+
+      TimeSeriesPlot()
+    })
+
+
+
+
+    SpatialPlot <- shiny::reactive({
+
+      tmap::tmap_mode('view')
+      tmap::tmap_options(check.and.fix = TRUE)
+
+      tm_shape(dataForSpatialPlot())+
+        tm_polygons(col=variable(),breaks = ValuesForLegend())
+    })
+    output$SpatialPlot <- tmap::renderTmap({
+
+      SpatialPlot()
+    })
+
+
+
+    formatDownloadTimeSeriesPlot <- shiny::reactive({
+
+      shiny::req(input$FormatDownloadTimeSeriesPlot)
+      input$FormatDownloadTimeSeriesPlot
+    })
+
+    formatDownloadSpatialPlot <- shiny::reactive({
+
+      shiny::req(input$FormatDownloadSpatialPlot)
+      input$FormatDownloadSpatialPlot
+    })
+
+
+    output$DownloadTimeSeriesPlot <- shiny::downloadHandler(
+
+      filename = function(){
+
+        paste("timeSeriesPlot.",formatDownloadTimeSeriesPlot(),sep='')
+      }
+      ,
+      content = function(file){
+
+        if(formatDownloadTimeSeriesPlot() == "html"){
+
+          htmlwidgets::saveWidget(TimeSeriesPlot(),file = file)
+
+        }
+        else{
+
+          htmlwidgets::saveWidget(TimeSeriesPlot(),file="temp.html")
+          webshot::webshot(url="temp.html",file=file)
+        }
+
+      }
+
+    )
+
+
+
+
+    output$DownloadSpatialPlot <- shiny::downloadHandler(
+
+      filename = function(){
+
+        paste("spatialPlot.",formatDownloadSpatialPlot(),sep='')
+      }
+      ,
+      content = function(file){
+
+        tmap::tmap_save(SpatialPlot(),file=file)
+
+      }
+
+    )
+
+    output$error <- shiny::renderText({
+
+      shiny::validate(
+        validate_dataFile(dataFileName())%then%
+          validate_dataFormat(dataFileName()),
+        validate_ShapeFile(shapeFileName())%then%
+          validate_mapFormat(shapeFileName()),
+        validate_mapRegNameCol(mapRegNameCol(),map())%then%
+          validate_dataRegNameCol(dataRegNameCol(),data(),updatedMap())
+      )
+    })
+
+
+
+
+
+  }
+
+  shiny::shinyApp(ui = ui, server = server)
+
+}
---
+++ VisualizzazioneDatiSpazioTemporali/R/utilityFunctions.R
@@ -0,0 +1,146 @@
+
+library(tmap)
+
+
+loadDataFile <- function(filename){
+
+  extension <- tools::file_ext(filename$datapath)
+  if(extension =='csv'){
+
+    data <- read.csv(filename$datapath)
+  }
+  if(extension=='xlsx'){
+
+    data <- readxl::read_excel(filename$datapath)
+  }
+
+  names(data)[1] = "date"
+  data$date <- as.Date(data$date)
+
+  return(data)
+
+}
+
+
+loadShapeFile<- function(InputFilesdf){
+
+  if(length(InputFilesdf$datapath)==1){
+    extension <- tools::file_ext(InputFilesdf$datapath)
+    map <- NULL
+    if(extension== 'RData'){
+      a <- load(InputFilesdf$datapath)
+      for(i in 1:length(a)){
+
+        if(class(get(a[i]))[1] %in% list('sf','SpatialPolygonsDataFrame')){
+          map <- get(a[i])
+          break
+        }
+      }
+    }
+  }
+  else{
+    tempdirname <- dirname(InputFilesdf$datapath[1])
+
+    # Rename files
+    for (i in 1:nrow(InputFilesdf)) {
+      file.rename(
+        InputFilesdf$datapath[i],
+        paste0(tempdirname, "/", InputFilesdf$name[i])
+      )
+    }
+    map <- sf::st_read(paste(tempdirname,
+                         InputFilesdf$name[grep(pattern = "*.shp$", InputFilesdf$name)],
+                         sep = "/"
+    ))
+
+  }
+
+  return(map)
+
+
+}
+
+
+
+renameColumn <- function(df,oldName,newName){
+
+  names(df)[names(df) == oldName] <- newName
+  return(df)
+
+}
+
+generateDataForSpatialPlotRegion <- function(data,map,variable,date){
+
+  df <- data[data$date == date,c(variable,"region_name")]
+  m <- map[,c("region_name")]
+  merged <- merge(df,m,by="region_name")
+
+  return(sf::st_as_sf(merged))
+
+
+}
+
+
+
+generateDataForSpatialPlotLocation <- function(data,map,variable,date){
+
+  df <- data[data$date == date,c(variable,"location_name")]
+  m <- map[,c("location_name")]
+  merged <- merge(df,m,by="location_name")
+
+  return(sf::st_as_sf(merged))
+
+
+}
+
+
+generateDataForTimeSeriesPlotRegion <- function(data,variable,setOfLocations){
+
+  if("all" %in% setOfLocations){
+    s <- utils::unstack(data[,c(variable,"region_name")])
+  }else{
+
+    s <- utils::unstack(data[data$region_name %in% setOfLocations,c(variable,'region_name')])
+  }
+  s[,"date"] <- (unique(data$date))
+  a <- xts::xts(x=s[,-ncol(s)],order.by=s$date)
+  return(a)
+
+}
+
+
+
+
+generateDataForTimeSeriesPlotLocation <- function(data,variable,setOfLocations){
+
+  if("all" %in% setOfLocations){
+    s <- utils::unstack(data[,c(variable,"location_name")])
+  }else{
+
+    s <- utils::unstack(data[data$location_name %in% setOfLocations,c(variable,'location_name')])
+  }
+  s[,"date"] <- (unique(data$date))
+  a <- xts::xts(x=s[,-ncol(s)],order.by=s$date)
+  return(a)
+
+}
+
+
+
+
+get_bins <- function(x,data,variable){
+
+  if(x==''){
+    x_min <- min(data[,variable])
+    x_max <- max(data[,variable])
+    interval <- (x_max-x_min)/6
+
+    return(c(x_min,x_min+interval,x_min+2*interval,x_min+3*interval,x_min+4*interval,x_max))
+  }else{
+    return(as.numeric(unlist(strsplit(x,","))))
+  }
+}
+
+
+
+
